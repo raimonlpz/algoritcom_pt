bd87a4f9468e4316953a11d198ba49b2ddeea815
Raimon López <raimon@bokokode.com>
2024-10-15 09:21:39 +0200

    added vercel config

--- client/src/api/SocketProvider.jsx
+++ client/src/api/SocketProvider.jsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client'
 import { playersAtom, mapAtom, userAtom, eventsPlayerAtom, drunkieAtom } from '../atoms'
 import { useAtom } from 'jotai'
 
-export const socket = io('http://localhost:3001')
+export const socket = io('https://algoritcom-pt.vercel.app:3001')
 
 export const SocketProvider = () => {
 
